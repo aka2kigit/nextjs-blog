bbf0ff51845efd43b929f4b4a0a9180f3d8c68c8
aka2kigit <shikaku.aka2ki@gmail.com>
2021-12-23 21:07:26 +0900

    gtag change

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import GoogleAnalytics from "../components/GoogleAnlytics";
+import usePageView from "../hooks/usePageView";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  usePageView();
   return (
     <>
       <GoogleAnalytics />
