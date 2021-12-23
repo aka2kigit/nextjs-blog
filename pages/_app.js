5cc4617a9686a40e1ed2df1ea2ee799a238c3ec2
aka2kigit <shikaku.aka2ki@gmail.com>
2021-12-23 21:22:35 +0900

    gtag delete

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,8 @@
-import GoogleAnalytics from "../components/GoogleAnlytics";
-import usePageView from "../hooks/usePageView";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  usePageView();
-
   return (
     <>
-      <GoogleAnalytics />
-
       <Component {...pageProps} />
     </>
   );
